bc18d352983634f2fead8a763866daecbf90a135
Petras Saduikis <petras@petras.co.uk>
2018-02-14 13:38:08 +0000

    initial commit

---
+++ deep_autoencoder.py
@@ -0,0 +1,85 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.datasets import mnist
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Deep Autoencoder
+
+# this is the size of our encoded representations
+encoding_dim = 32   # 32 floats -> compression factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+input_img = Input(shape=(784, ))
+
+my_epochs = 100
+
+# "encoded" is the encoded representation of the inputs
+encoded = Dense(encoding_dim * 4, activation='relu')(input_img)
+encoded = Dense(encoding_dim * 2, activation='relu')(encoded)
+encoded = Dense(encoding_dim, activation='relu')(encoded)
+
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(encoding_dim * 2, activation='relu')(encoded)
+decoded = Dense(encoding_dim * 4, activation='relu')(decoded)
+decoded = Dense(784, activation='sigmoid')(decoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input_img, decoded)
+
+# Separate Encoder model
+
+# this model maps an input to its encoded representation
+encoder = Model(input_img, encoded)
+
+# Separate Decoder model
+
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim, ))
+# retrieve the layers of the autoencoder model
+decoder_layer1 = autoencoder.layers[-3]
+decoder_layer2 = autoencoder.layers[-2]
+decoder_layer3 = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer3(decoder_layer2(decoder_layer1(encoded_input))))
+
+# Train to reconstruct MNIST digits
+
+# configure model to use a per-pixel binary crossentropy loss, and the Adadelta optimizer
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+# prepare input data
+(x_train, _), (x_test, _) = mnist.load_data()
+
+# normalize all values between 0 and 1 and flatten the 28x28 images into vectors of size 784
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print(x_train.shape)
+print(x_test.shape)
+
+# Train autoencoder for 50 epochs
+
+autoencoder.fit(x_train, x_train, epochs=my_epochs, batch_size=256, shuffle=True, validation_data=(x_test, x_test))
+
+# after 100 epochs the autoencoder seems to reach a stable train/test lost value
+
+# Visualize the reconstructed encoded representations
+
+# encode and decode some digits
+# note that we take them from the *test* set
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(10, 2), dpi=100)
+for i in range(n):
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+plt.show()
---
+++ simple_autoencoder.py
@@ -0,0 +1,89 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras import regularizers
+from keras.datasets import mnist
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Single fully-connected neural layer as encoder and decoder
+
+use_regularizer = False
+my_regularizer = None
+my_epochs = 50
+
+if use_regularizer:
+    # add a sparsity constraint on the encoded representations
+    my_regularizer = regularizers.l1(10e-5)
+    # and a larger number of epochs as the added regularization the model
+    # is less likely to overfit and can be trained longer
+    my_epochs = 100
+
+# this is the size of our encoded representations
+encoding_dim = 32   # 32 floats -> compression factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+input_img = Input(shape=(784, ))
+
+# "encoded" is the encoded representation of the inputs
+encoded = Dense(encoding_dim, activation='relu', activity_regularizer=my_regularizer)(input_img)
+
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input_img, decoded)
+
+# Separate Encoder model
+
+# this model maps an input to its encoded representation
+encoder = Model(input_img, encoded)
+
+# Separate Decoder model
+
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+# Train to reconstruct MNIST digits
+
+# configure model to use a per-pixel binary crossentropy loss, and the Adadelta optimizer
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+# prepare input data
+(x_train, _), (x_test, _) = mnist.load_data()
+
+# normalize all values between 0 and 1 and flatten the 28x28 images into vectors of size 784
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print(x_train.shape)
+print(x_test.shape)
+
+# Train autoencoder for 50 epochs
+
+autoencoder.fit(x_train, x_train, epochs=my_epochs, batch_size=256, shuffle=True, validation_data=(x_test, x_test))
+
+# after 50/100 epochs the autoencoder seems to reach a stable train/test lost value
+
+# Visualize the reconstructed encoded representations
+
+# encode and decode some digits
+# note that we take them from the *test* set
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(10, 2), dpi=100)
+for i in range(n):
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+plt.show()
