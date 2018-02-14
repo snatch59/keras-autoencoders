04ba4daa7899f54ab2827e1a565383e87b4b4605
Petras Saduikis <petras@petras.co.uk>
2018-02-14 17:42:34 +0000

    added image denoising + updates

--- image_denoising.py
+++ image_denoising.py
@@ -8,20 +8,16 @@ import matplotlib.pyplot as plt
 
 input_img = Input(shape=(28, 28, 1))    # adapt this if using 'channels_first' image data format
 
-x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
 x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
 encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-# at this point the representation is (4, 4, 8), i.e. 128-dimensional
+# at this point the representation is (7, 7, 32)
 
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
 x = UpSampling2D((2, 2))(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(16, (3, 3), activation='relu')(x)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
 x = UpSampling2D((2, 2))(x)
 decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
@@ -38,11 +34,19 @@ x_test = x_test.astype('float32') / 255.
 x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))    # adapt this if using 'channels_first' image data format
 x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))       # adapt this if using 'channels_first' image data format
 
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
+
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
 # open a terminal and start TensorBoard to read logs in the autoencoder subdirectory
 # tensorboard --logdir=autoencoder
 
-autoencoder.fit(x_train, x_train, epochs=50, batch_size=128, shuffle=True, validation_data=(x_test, x_test),
-                callbacks=[TensorBoard(log_dir='conv_autoencoder')])
+autoencoder.fit(x_train_noisy, x_train, epochs=100, batch_size=128, shuffle=True,
+                validation_data=(x_test_noisy, x_test),
+                callbacks=[TensorBoard(log_dir='noisy', histogram_freq=0, write_grads=False)])
 
 # take a look at the reconstructed digits
 decoded_imgs = autoencoder.predict(x_test)
@@ -50,9 +54,9 @@ decoded_imgs = autoencoder.predict(x_test)
 n = 10
 plt.figure(figsize=(10, 4), dpi=100)
 for i in range(n):
-    # display original
+    # display noisy
     ax = plt.subplot(2, n, i + 1)
-    plt.imshow(x_test[i].reshape(28, 28))
+    plt.imshow(x_test_noisy[i].reshape(28, 28))
     plt.gray()
     ax.set_axis_off()
 
@@ -74,7 +78,7 @@ n = 10
 plt.figure(figsize=(10, 4), dpi=100)
 for i in range(n):
     ax = plt.subplot(1, n, i + 1)
-    plt.imshow(encoded_imgs[i].reshape(4, 4 * 8).T)
+    plt.imshow(encoded_imgs[i].reshape(7, 7 * 32).T)
     plt.gray()
     ax.set_axis_off()
 
